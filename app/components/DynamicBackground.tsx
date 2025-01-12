97e27c0c817c5bc0d3c9d4fdfbbce9a1ea51b24e
zach-karlovich <ddf4me@virginia.edu>
2025-01-12 11:39:16 -0500

    Fixed type error in DynamicBackground

--- app/components/DynamicBackground.tsx
+++ app/components/DynamicBackground.tsx
@@ -26,15 +26,19 @@ const DynamicBackground = () => {
       speedY: number
       color: string
       alpha: number
-      
-      constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
-        this.size = Math.random() * 5 + 3
-        this.speedX = Math.random() * 2 - 1
-        this.speedY = Math.random() * 2 - 1
-        this.alpha = Math.random() * 0.075 + 0.01
-        
+      private canvas: HTMLCanvasElement
+      private ctx: CanvasRenderingContext2D
+
+      constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.x = Math.random() * this.canvas.width;
+        this.y = Math.random() * this.canvas.height;
+        this.size = Math.random() * 5 + 3;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
+        this.alpha = Math.random() * 0.075 + 0.01;
+
         const nordColors = [
           'rgba(136, 192, 208, 0.05)',
           'rgba(143, 188, 187, 0.05)',
@@ -48,34 +52,34 @@ const DynamicBackground = () => {
         this.x += this.speedX
         this.y += this.speedY
 
-        if (this.x < 0 || this.x > canvas.width) this.speedX *= -1
-        if (this.y < 0 || this.y > canvas.height) this.speedY *= -1
+        if (this.x < 0 || this.x > this.canvas.width) this.speedX *= -1
+        if (this.y < 0 || this.y > this.canvas.height) this.speedY *= -1
 
         this.alpha = Math.sin(Date.now() * 0.001) * 0.2 + 0.3
       }
 
       draw() {
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-        ctx.fillStyle = this.color.replace(/[\d.]+\)$/g, `${this.alpha})`)
-        ctx.fill()
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        this.ctx.fillStyle = this.color.replace(/[\d.]+\)$/g, `${this.alpha})`)
+        this.ctx.fill()
 
-        ctx.shadowBlur = 25
-        ctx.shadowColor = this.color
-        
+        this.ctx.shadowBlur = 25
+        this.ctx.shadowColor = this.color
+
         particles.forEach(particle => {
           const dx = this.x - particle.x
           const dy = this.y - particle.y
           const distance = Math.sqrt(dx * dx + dy * dy)
-          
+
           if (distance < 200) {
-            ctx.beginPath()
-            const opacity = (1 - distance/200) * this.alpha
-            ctx.strokeStyle = this.color.replace(/[\d.]+\)$/g, `${opacity})`)
-            ctx.lineWidth = 1.5
-            ctx.moveTo(this.x, this.y)
-            ctx.lineTo(particle.x, particle.y)
-            ctx.stroke()
+            this.ctx.beginPath()
+            const opacity = (1 - distance / 200) * this.alpha
+            this.ctx.strokeStyle = this.color.replace(/[\d.]+\)$/g, `${opacity})`)
+            this.ctx.lineWidth = 1.5
+            this.ctx.moveTo(this.x, this.y)
+            this.ctx.lineTo(particle.x, particle.y)
+            this.ctx.stroke()
           }
         })
       }
@@ -83,16 +87,16 @@ const DynamicBackground = () => {
 
     const init = () => {
       for (let i = 0; i < particleCount; i++) {
-        particles.push(new Particle())
+        particles.push(new Particle(canvas, ctx))
       }
     }
 
     const animate = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
-      
+
       ctx.fillStyle = 'rgba(26, 29, 35, 0.999)'
       ctx.fillRect(0, 0, canvas.width, canvas.height)
-      
+
       for (let i = 0; i < particles.length; i++) {
         particles[i].update()
         particles[i].draw()
@@ -116,10 +120,10 @@ const DynamicBackground = () => {
   }, [])
 
   return (
-    <canvas 
-      ref={canvasRef} 
+    <canvas
+      ref={canvasRef}
       className="fixed top-0 left-0 w-full h-full"
-      style={{ 
+      style={{
         zIndex: -1,
         backgroundColor: '#050607',
       }}
